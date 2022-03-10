6f929eeb630ee076e5ad8c4722cd78faf005d637
lucky world <lucky@villat.guillaume@gmail.com>
2022-03-10 17:19:38 +0100

    stash

--- p1/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo docker build -t guvillat_host -f ./guvillat_host/Dockerfile .
-sudo docker build -t guvillat_routeur -f ./guvillat_routeur/Dockerfile .
-gns3server&
-gns3 p1.gns3project&
