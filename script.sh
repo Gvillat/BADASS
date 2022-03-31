7fadd7b560d45567805ac2d36cceb95a9d1ff1a2
lucky world <lucky@villat.guillaume@gmail.com>
2022-03-31 06:26:08 +0200

    ok

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sudo docker build -t guvillat_host -f ./p1/guvillat_host/Dockerfile .
 sudo docker build -t guvillat_routeur -f ./p1/guvillat_routeur/Dockerfile .
-#gns3server&
-#gns3 p1/p1.gns3project&
-#gns3 p2/p2.gns3project&
-#gns3 p3/p3.gns3project&
+gns3server&
